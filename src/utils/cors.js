b17fa515b3cbf8e3ed691696dd967b74744c0620
Tien Nguyen Thanh <tien.nguyenthanh@pycogroup.com>
2018-09-16 08:58:50 +0700

    Update cors

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,6 +1,9 @@
 import { whitelist } from 'config/cors';
 
 const handleOrigin = (origin, callback) => {
+  // allow requests with no origin (like mobile apps or curl requests)
+  if(!origin) return callback(null, true);
+
   if (whitelist.indexOf(origin) === -1) {
     return callback(new Error('Not allowed by CORS'), false);
   }
